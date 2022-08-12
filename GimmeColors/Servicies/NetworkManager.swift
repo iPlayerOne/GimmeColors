151fd2fe93517052caea718c281f3567bb384514
Ivan Korobov <ivan.koroboff@gmail.com>
2022-08-12 21:46:46 +0200

    seeking

--- GimmeColors/Servicies/NetworkManager.swift
+++ GimmeColors/Servicies/NetworkManager.swift
@@ -8,8 +8,14 @@
 import Foundation
 
 enum Link: String {
-    case singleURL = "https://www.thecolorapi.com/id?rgb=\(r),\(g),\(b)"
-    case schemeURL = "https://www.thecolorapi.com/scheme?rgb=\(r),\(g),\(b)"
+    case singleURL = "https://www.thecolorapi.com/id?rgb="
+    case schemeURL = "https://www.thecolorapi.com/scheme?rgb="
+}
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingError
 }
 
 class NetworkManager {
@@ -17,5 +23,33 @@ class NetworkManager {
     
     private init() {}
     
+    func getURLString(for link: Link, r: Int, g: Int, b: Int) -> String {
+        let newUrl = "\(link.rawValue)\(r),\(g),\(b)"
+        return newUrl
+    }
     
+    
+    
+    func fetch<T: Decodable>(_ type: T.Type, from url: String?, completion: @escaping(Result<T,NetworkError>) -> Void) {
+        guard let url = URL(string: url ?? "") else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                let type = try decoder.decode(T.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(type))
+                }
+            } catch {
+                completion(.failure(.decodingError))
+            }
+        }.resume()
+    }
 }
--- GimmeColors/ViewControlles/PalleteViewController.swift
+++ GimmeColors/ViewControlles/PalleteViewController.swift
@@ -14,51 +14,82 @@ class PalleteViewController: UIViewController {
     @IBOutlet weak var colorInfoLabel: UILabel!
     @IBOutlet weak var palleteCollection: UICollectionView!
     
-    var color: Color?
-    var scheme: Scheme?
-    
-    private let randomURL = "https://www.colourlovers.com/api/palettes/random?format=json"
+    private var singleColor: Color?
+    private var schemeColor: Scheme? {
+        didSet {
+            palleteCollection.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getRandomPallete()
+        
+        getColor()
+        print("didLoad\(singleColor)")
+        getScheme()
+        palleteCollection.reloadData()
+        print("didload \(schemeColor)")
+        print("didload \(schemeColor?.colors?.count)")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getColor()
+        print("willAppear\(singleColor)")
+        getScheme()
+        palleteCollection.reloadData()
+        print(" willappear \(schemeColor)")
+        print("willappear \(schemeColor?.colors?.count)")
     }
     
     @IBAction func colorViewPressed() {
         
     }
     
-    private func getRandomPallete() {
-        guard let url = URL(string: randomURL) else { return }
-        
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data else {
-                print(error?.localizedDescription ?? "No error description")
-                return
+
+    
+    private func getColor() {
+        NetworkManager.shared.fetch(Color.self, from: NetworkManager.shared.getURLString(for: .singleURL, r: 0, g: 0, b: 99)) { [weak self] result in
+            switch result {
+                case .success(let color):
+                    print(color)
+                    self?.singleColor = color
+                case .failure(let error):
+                    print(error)
             }
-            
-            do {
-                let pallete = try JSONDecoder().decode([Pallete].self, from: data)
-                print(pallete)
-            } catch let error {
-                print(error)
-            }
-            
-        }.resume()
+        }
     }
     
+    private func getScheme() {
+        NetworkManager.shared.fetch(Scheme.self, from: NetworkManager.shared.getURLString(for: .schemeURL, r: 0, g: 0, b: 99)) { [weak self] result in
+            switch result {
+                case .success(let scheme):
+                    print(scheme)
+                    self?.schemeColor = scheme
+                case .failure(let error):
+                    print(error)
+            }
+        }
+        
+    }
 }
-
-extension PalleteViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     
+extension PalleteViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        schemeColor?.colors?.count ?? 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "schemeCell", for: indexPath)
+        let cellColor = schemeColor?.colors?[indexPath.item].rgb?.value
+        
+        print("Ячейка \(cellColor)")
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Дид селект \(schemeColor)")
     }
     
     
 }
-
